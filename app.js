7c14d76b70eafa3c4df1d0cc51f9c0798deab8b7
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2024-07-08 15:30:03 +0900

    made authentification

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ db.connect((err) => {
 //      .query(MySQLに出したい命令, 代入したいパラメタ配列, コールバック関数)
 //      例）.query( '(?, ?)', [A, B], ... )とすると、
 //      (?, ?) = (A, B)となるように配列の各要素が順に?に代入される 
+/*
 function insertUser(mailAddress, password, callback) {
     const insertQuery = 'INSERT INTO users (email, password) VALUES (?, ?)';
     db.query(insertQuery, [mailAddress, password], (err, result) => {
@@ -61,6 +62,7 @@ function insertUser(mailAddress, password, callback) {
         callback(null, result);
     });
 }
+*/
 
 
 
@@ -71,39 +73,83 @@ app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
-
-/* =========== CONTACT =========== */
-app.get('/contact', function (req, res) {
-    res.render('pages/contact');
+// /index ルートを追加
+app.get('/index', (req, res) => {
+    res.render('pages/index'); // ここで index.ejs をレンダリングする
 });
 
+
+/* =========== CONTACT =========== */
+/*
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+*/
+
+/*
+const isAuthorized = (mailAddress) => {
+    let overlapCheckQuery = `SELECT * FROM ${TABLE_NAME} WHERE email = ?`;
+    db.query(overlapCheckQuery, [mailAddress]);
+}
+*/
+
+
+
 // contactページでポストされた情報が来て欲しい
-app.post('/contact', (req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+
+    // フォームからrequestされたメアドとパスワードを格納する
     let mailAddress = req.body.mailAddress;
     let password = req.body.password;
-    console.log(mailAddress, password);
-    // postされたemailとDBのemailの重複チェック
+
+    // email passwordといったユーザ情報をMySQLから取り出す
     let overlapCheckQuery = `SELECT * FROM ${TABLE_NAME} WHERE email = ?`;
-    db.query(overlapCheckQuery, [mailAddress], (err, result) => {
+    db.query(overlapCheckQuery, [mailAddress, password], (err, result) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        if (result.length > 0) {
-            console.log("The user has already registered.")
-            return;
+        // 取り出したユーザ情報
+        let emaiInDb = result[0].email;
+        let passwordInDb = result[0].password;
+        
+        console.log(mailAddress);
+        console.log(emaiInDb);
+        console.log(password);
+        console.log(passwordInDb);
+
+        // アドレスが一致していない => Unauthorized
+        // アドレス一致
+        //      -> パスワ一致してない => passwordが違いますと表示させて、同じページをresponse
+        //      -> パスワードが一致 => index pageへ
+        if ( emaiInDb !== mailAddress ) {
+            return res.send("<h1>401 Unauthorized</h1>");
+        } 
+        if ( passwordInDb !== password) {
+            return res.send("<h1>Password is wrong.</h1>");
+        } else {
+            console.log("Authorized");
+            res.render('pages/portfolio');    // TODO: Index pageに送ることができない！
         }
 
+        
+        // Passwordが違う
+
+        /*
+         * NOTE(2024/07/08): もし認証ユーザ以外を弾くならば、今の所、新規ユーザをDBへ格納する必要はない
+         */
+        
         // emailとpasswordをDBへ格納する関数
+        /*
         insertUser(mailAddress, password, (err, result) => {
             if (err) {
                 return res.status(500).send("Error inserting data into database");
             }
             res.render('pages/contact');             
         });
+        */
+        
 
     })
-
     //res.render('pages/contact');
 })
 
